9561758b68f2f3a260d528f92fd25b80aea8a292
ethan-green3 <ethangreen2000@yahoo.com>
2025-05-02 15:35:55 -0500

    FInal commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import count_words
 from stats import count_characters
 
@@ -8,8 +10,28 @@ def get_book_text(file_path):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    len_args = len(sys.argv)
+    if len_args != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file = sys.argv[1] 
+
+    if book_file == "":
+        print("Please provide a book file to analyze")
+        sys.exit(1)
+    print(book_file)
+    book_text = get_book_text(book_file)
+    word_count = count_words(book_text)
     count_of_each_character = count_characters(book_text)
-    print(count_of_each_character)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at ")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("---------- Character Count -------")
+    for character, count in count_of_each_character:
+        if not character.isalpha():
+            continue
+        print(f"{character}: {count}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,4 +10,9 @@ def count_characters(text):
             count_of_each_character[character] += 1
         else:
             count_of_each_character[character] = 1
+
+    count_of_each_character = sorted(count_of_each_character.items(), reverse=True, key=sort_dictionary)
     return count_of_each_character
+
+def sort_dictionary(item):
+    return item[1]
